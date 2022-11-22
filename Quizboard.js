1a4446f10089c15fceeae85e4d7c9f99842f3eea
Sedad Deeg <sdeeg@tso.de>
2022-11-22 10:50:04 +0100

    Quizboard2

--- Quizboard.js
+++ Quizboard.js
@@ -1,63 +1,68 @@
-var basicTimeline = anime.timeline({
-    autoplay: false
-  });
+// var basicTimeline = anime.timeline({
+//     autoplay: false
+//   });
   
-  var pathEls = $(".check");
-  for (var i = 0; i < pathEls.length; i++) {
-    var pathEl = pathEls[i];
-    var offset = anime.setDashoffset(pathEl);
-    pathEl.setAttribute("stroke-dashoffset", offset);
-  }
+  // var pathEls = $(".check");
+  // for (var i = 0; i < pathEls.length; i++) {
+  //   var pathEl = pathEls[i];
+  //   var offset = anime.setDashoffset(pathEl);
+  //   pathEl.setAttribute("stroke-dashoffset", offset);
+  // }
+
+  var ScoreTeam1 = 0, ScoreTeam2 = 0, ScoreTeam3 = 0, ScoreTeam4 = 0, ScoreTeam5 = 0;
+
+//
+  document.getElementById(ScoreTeam1).innerHTML = ScoreTeam1;
+//
+  // basicTimeline
+  //   .add({
+  //     targets: ".text",
+  //     duration: 1,
+  //     opacity: "0"
+  //   })
+  //   .add({
+  //     targets: ".button1",
+  //     duration: 1300,
+  //     height: 10,
+  //     width: 300,
+  //     backgroundColor: "#2B2D2F",
+  //     border: "0",
+  //     borderRadius: 100
+  //   })
+    // .add({
+    //   targets: ".progress-bar",
+    //   duration: 2000,
+    //   width: 300,
+    //   easing: "linear"
+    // })
+    // .add({
+    //   targets: ".button1",
+    //   width: 0,
+    //   duration: 1
+    // })
+    // .add({
+    //   targets: ".progress-bar",
+    //   width: 80,
+    //   height: 80,
+    //   delay: 500,
+    //   duration: 750,
+    //   borderRadius: 80,
+    //   backgroundColor: "#71DFBE"
+    // })
+    // .add({
+    //   targets: pathEl,
+    //   strokeDashoffset: [offset, 0],
+    //   duration: 200,
+    //   easing: "easeInOutSine"
+    // });
   
-  basicTimeline
-    .add({
-      targets: ".text",
-      duration: 1,
-      opacity: "0"
-    })
-    .add({
-      targets: ".button1",
-      duration: 1300,
-      height: 10,
-      width: 300,
-      backgroundColor: "#2B2D2F",
-      border: "0",
-      borderRadius: 100
-    })
-    .add({
-      targets: ".progress-bar",
-      duration: 2000,
-      width: 300,
-      easing: "linear"
-    })
-    .add({
-      targets: ".button1",
-      width: 0,
-      duration: 1
-    })
-    .add({
-      targets: ".progress-bar",
-      width: 80,
-      height: 80,
-      delay: 500,
-      duration: 750,
-      borderRadius: 80,
-      backgroundColor: "#71DFBE"
-    })
-    .add({
-      targets: pathEl,
-      strokeDashoffset: [offset, 0],
-      duration: 200,
-      easing: "easeInOutSine"
-    });
+  // $(".button").click(function() {
+  //   basicTimeline.play();
+  // });
   
-  $(".button").click(function() {
-    basicTimeline.play();
-  });
-  
-  $(".text").click(function() {
-    basicTimeline.play();
-  });
+  // $(".text").click(function() {
+  //   basicTimeline.play();
+  // });
 
   function name(params) {
     
---
+++ Quizboard2.js
@@ -0,0 +1,5 @@
+var team1score = 15;
+
+function DisplayTeam1Score(params) {
+    document.getElementById("test1").innerHTML = number;
+}
